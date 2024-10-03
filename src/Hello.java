04adfdc8a9e023ec59d423fa311d711486ffa635
DanSal11 <salistean.d3@gmail.com>
2024-10-03 21:08:52 +0300

    My first java lesson

---
+++ src/Hello.java
@@ -0,0 +1,53 @@
+public class Hello {
+//psvm, sout
+    public static void main(String[] args) {
+
+        int myFirstVariable = 100;
+        int mySecondVariable = 0;
+        final int MAX_SIZE = 10;
+        final double PI = Math.PI;
+
+        System.out.println("Hello world !");
+
+        System.out.println("Seconds variable value is " + mySecondVariable);
+
+        System.out.println(args[0] + args[1]);
+
+        int operandLeft = Integer.parseInt(args[0]);
+        int operandRight = Integer.parseInt(args[1]);
+
+        System.out.println(operandLeft + operandRight);
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+
+        String currentDay = "Monday";
+
+        switch (currentDay) {
+            case "Monday": {
+                System.out.println("Noo Monday!");
+                break;
+            }
+            case "Friday": {
+                System.out.println("Yeee Friday !!");
+                break;
+            }
+            default: {
+                System.out.println("Meeeh");
+            }
+        }
+
+        int currentNumber = 1;
+        while ((currentNumber <= MAX_SIZE)){
+            System.out.println("Current number is " + currentNumber);
+            currentNumber++;
+        }
+
+        for (int i = 0; i <= MAX_SIZE ; i++) {
+            System.out.println("Current number is " + i);
+        }
+
+        for (String arg: args){
+            System.out.println(arg);
+        }
+
+    }
+}
